e62d8af15281f676eaabefed6976404d37e300aa
Talip Can Korkmazer <talip.korkmazer@fonq.nl>
2025-06-30 02:42:28 +0300

    force https added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,6 +34,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
